b2a88c52a07d234951c656d7c21ee1ad7e4d3377
dhana.arvina <dhana.arvina@01-18-DHANAARVI>
2023-02-08 11:24:41 +0700

    first commit

---
+++ src/main/java/com/dhana/pipelinetesting/PipelineTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dhana.pipelinetesting;
+
+/**
+ *
+ * @author dhana.arvina
+ */
+public class PipelineTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
